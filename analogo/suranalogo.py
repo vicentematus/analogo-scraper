56bdd17b2e4d11aeb7960ffd284c2632365682e1
vicentematus <vicentematusortiz@gmail.com>
2021-06-28 17:19:47 -0400

    cambios

--- analogo/suranalogo.py
+++ analogo/suranalogo.py
@@ -1,23 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 from urllib.request import urlopen, Request
-import csv
-import sys
+import pandas as pd
+from request_html import HTMLSession
 print('Imprimiendo productos de suranalogo')
 
 
 url = requests.get("https://suranalogo.cl/pelicula-b/n-35mm").text
 soup = BeautifulSoup(url, 'html.parser')
 
-file = open('suranalogoproductos.csv', 'w', newline='')
-writer = csv.writer(file)
 
-writer.writerow(["Nombre", "Precio", "Stock"])
-
-
+lista_rollos = []
 rollos = soup.find_all('div', class_="col-lg-3 col-md-3 col-6")
 
+
 for rollo in rollos:
+
     nombre = rollo.find('div', class_="caption").h4.a.text.replace('ROLLO BLANCO Y NEGRO', '').replace(
         'ROLLO PELÍCULA', '').replace('ROLLO PELICULA FOTOGRAFICA', '').strip()
     precio = rollo.find('div', class_="list-price").text.strip()
@@ -27,11 +25,17 @@ for rollo in rollos:
         stock = "FALSE"
     else:
         stock = "TRUE"
-#     print(nombre)
-#     print(precio)
-#     print(agotado.value)
-    writer.writerow(
-        [nombre, precio, stock])
+
+    rollo_informacion = {
+        'nombre': nombre,
+        'precio': precio,
+        'stock': stock
+    }
+
+    lista_rollos.append(rollo_informacion)
 
 
-file.close()
+df = pd.DataFrame(lista_rollos)
+print(df.head())
+
+df.to_csv('suranalogo-blanco-negro.csv')
