31876a1ba58699369ad48becfb3266771c0dd15d
vicentematus <38754555+vicentematus@users.noreply.github.com>
2021-06-29 04:16:24 -0400

    Sur analogo paginacion correcta, y scrape de los productos

---
+++ analogo/suranalogo-2.py
@@ -0,0 +1,87 @@
+from bs4 import BeautifulSoup
+import requests
+from requests_html import HTMLSession
+import pandas as pd
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+
+url = "https://suranalogo.cl/pelicula-35mm"
+
+
+s = HTMLSession()
+
+peliculas_35mm_paginas = []
+rollos_35mm = []
+
+
+def getdata(url):
+    r = s.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    return soup
+
+
+def proxima_pagina(soup):
+    page = soup.find('div', {'class': 'category-pager'})
+    paginas = page.find_all('span')
+    if paginas[0].text != paginas[2].text:
+        print(paginas[0].text != paginas[2])
+        url = 'https://suranalogo.cl' + \
+            str(page.select('div > a')[1]['href'])
+        getInfo(url)
+        peliculas_35mm_paginas.append(url)
+        return url
+    else:
+        return
+
+
+# def peliculas():
+#     categorias = [f'{url}/pelicula-b/n-35mm/',
+#                   f'{url}/pelicula-b/n-format0-120/',
+#                   f'{url}/cargas/',
+#                   f'{url}/pelicula-35mm/color-35mm/']
+#     # for link in soup.find_all('li', class_='nav-item'):
+
+
+def getInfo(url):
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    print("entre al get info")
+    rollos = soup.find_all('div', class_="col-lg-3 col-md-3 col-6")
+
+    for rollo in rollos:
+        nombre = rollo.find('div', class_="caption").h4.a.text.replace('ROLLO BLANCO Y NEGRO', '').replace(
+            'ROLLO PELÍCULA', '').replace('ROLLO PELICULA FOTOGRAFICA', '').strip()
+        precio = rollo.find(
+            'div', class_="list-price").text.replace('$', '').replace('CLP', '').replace('.', '').strip()
+        stock = "none"
+        agotado = rollo.find('span', class_="status-tag")
+        url = "https://suranalogo.cl" + \
+            str(rollo.find('a', class_="product-image")['href'].strip())
+        if(agotado):
+            stock = "FALSE"
+        else:
+            stock = "TRUE"
+        informacion = {
+            "nombre": nombre,
+            "precio": precio,
+            "stock": stock,
+            "url": url
+        }
+        print(informacion)
+        rollos_35mm.append(informacion)
+    return
+
+
+while True:
+    soup = getdata(url)
+    url = proxima_pagina(soup)
+    if not url:
+        break
+
+print(peliculas_35mm_paginas)
+print(rollos_35mm)
+
+df = pd.DataFrame(rollos_35mm)
+print(df.head())
+
+df.to_csv('surnanalogo-35mm.csv')
